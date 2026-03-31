6739a9ab918a1cdaa0146f18def83d7959ab6fac
ianfx0110 <ianfx0110@gmail.com>
2026-03-31 16:03:14 +0300

    try

--- api.js
+++ api.js
@@ -1,1 +1,21 @@
-fetch()
+fetch("http://test.bosotournaments.com/teams")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    // use the data in the DOM - UI - html css
+    const table = document.querySelector("table")
+    data.forEach((team) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${team.name}</td>
+            <td>${team.sport}</td>
+            <td>${team.category}</td>
+        `;
+        table.appendChild(row);
+    })
+    
+  }
+);
+// CORS - Cross Origin Resource Sharing
+// Same Origin Policy - SOP
+// CORS is a security feature implemented by browsers to restrict web pages from making requests to a different domain than the one that served the web page. It allows servers to specify who can access their resources and how they can be accessed.
